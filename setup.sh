e6ea7a54a3a1526d546581c8438654e7949a3458
Raphaël Valyi <rvalyi@akretion.com>
2015-11-28 23:13:52 -0200

    ln fallback; patch liquid plugin for Locomotive .liquid.haml / .liquid.erb files

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if ! [[ -L $HOME/.vimrc ]]; then
 fi
 
 if ! [[ -L $HOME/.vim ]]; then
-  ln -s `pwd` $HOME/.vim
+  ln -s `pwd` $HOME/.vim || true
 fi
 
 echo
@@ -20,3 +20,10 @@ else
 fi
 
 vim +BundleInstall +qall
+
+
+# liquid fix for Locomotive *.liquid.haml files:
+if ! grep -qe "liquid\.haml" "$HOME/.vim/bundle/vim-liquid/ftdetect/liquid.vim"; then
+  echo "au BufNewFile,BufRead *.liquid.haml     set ft=liquid" >> "$HOME/.vim/bundle/vim-liquid/ftdetect/liquid.vim"
+  echo "au BufNewFile,BufRead *.liquid.erb      set ft=liquid" >> "$HOME/.vim/bundle/vim-liquid/ftdetect/liquid.vim"
+fi
